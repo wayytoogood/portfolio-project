39f35e0bc23959300b2eeb4627f8a4ebff8de494
wayytoogood <huseyinsuna1903@gmail.com>
2020-09-19 22:06:42 +0300

    first commit

--- js/app.js
+++ js/app.js
@@ -1,22 +1,22 @@
-const navbar = document.querySelector("#nav");
-const navBtn = document.querySelector("#nav-btn");
-const closeBtn = document.querySelector("#close-btn");
-const sidebar = document.querySelector("#sidebar");
-const date = document.querySelector("#date");
-// add fixed class to navbar
-window.addEventListener("scroll", function () {
-  if (window.pageYOffset > 80) {
-    navbar.classList.add("navbar-fixed");
+const sideNav = document.querySelector('.side-nav');
+const navToggle = document.querySelector('.nav-toggle');
+const navClose = document.querySelector('.nav-close');
+const nav = document.querySelector('.nav');
+
+navToggle.addEventListener('click', () => {
+  sideNav.classList.add('side-nav-open');
+});
+navClose.addEventListener('click', () => {
+  sideNav.classList.remove('side-nav-open');
+});
+
+const navHeight = nav.getBoundingClientRect().height + 12;
+console.log(nav, navHeight);
+window.addEventListener('scroll', () => {
+  const scrollHeight = window.pageYOffset;
+  if (scrollHeight > navHeight) {
+    nav.classList.add('nav-fixed');
   } else {
-    navbar.classList.remove("navbar-fixed");
+    nav.classList.remove('nav-fixed');
   }
 });
-// show sidebar
-navBtn.addEventListener("click", function () {
-  sidebar.classList.add("show-sidebar");
-});
-closeBtn.addEventListener("click", function () {
-  sidebar.classList.remove("show-sidebar");
-});
-// set year
-date.innerHTML = new Date().getFullYear();
